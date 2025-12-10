9917174c02dc1904091f585e5ffd4dd353c23da4
narges taghavi <ntghnarges@gmail.com>
2025-12-10 21:08:50 +0330

    Car class

---
+++ Car.java
@@ -0,0 +1,11 @@
+public class Car{
+    private int carId;
+    //O(1)
+    public Car(int carId){
+        this.carId = carId;
+    }
+    //O(1)
+    public int getCarId(){
+        return carId;
+    }
+}
